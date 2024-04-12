8f4ce0330b092e0858176e9e1e3b12ac8b7f02d5
danZxC <113315237+danZxC@users.noreply.github.com>
2024-04-12 19:13:56 +0300

    fff

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory} from 'vue-router'
 import ProductPage from '../views/ProductPage.vue'
 import GamePage from '../views/GamePage.vue'
 import MainPage from '../views/MainPage.vue'
@@ -31,7 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/shop'),
+  history: createWebHashHistory(),
   routes
 })
 
